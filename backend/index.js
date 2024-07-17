23becd5c42bb82c0a406e747ea48316ddb32c12a
Divine <divinetidy@gmail.com>
2024-07-17 13:20:54 +0100

    add

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookie = require("cookie-parser");
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(json());
 app.use(
   cors({
